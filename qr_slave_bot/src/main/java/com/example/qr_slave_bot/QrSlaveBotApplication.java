87b6617681797c774279495cfae0a9fe30648d08
danil12389 <akverrr@gmail.com>
2022-09-11 16:13:06 +0300

    Initial commit

---
+++ qr_slave_bot/src/main/java/com/example/qr_slave_bot/QrSlaveBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.qr_slave_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrSlaveBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrSlaveBotApplication.class, args);
+	}
+
+}
---
+++ qr_slave_bot/src/test/java/com/example/qr_slave_bot/QrSlaveBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qr_slave_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrSlaveBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
